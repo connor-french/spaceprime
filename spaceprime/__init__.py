4ad0fd9e4ee41acae3d7cadbdbc49bd6de5d31f1
Connor French <confrench33@gmail.com>
2024-04-15 11:26:56 -0400

    fix

--- spaceprime/__init__.py
+++ spaceprime/__init__.py
@@ -1,5 +1,12 @@
-"""Top-level package for spaceprime."""
+"""
+spaceprime is a python package to facilitate spatially explicit coalescent modeling in msprime.
+Please see the documentation at https://connor-french.github.io/spaceprime
+for more information.
+"""
 
 __author__ = """Connor French"""
 __email__ = "french.connor.m@gmail.com"
 __version__ = "0.0.2"
+
+
+from spaceprime.utilities import raster_to_demes

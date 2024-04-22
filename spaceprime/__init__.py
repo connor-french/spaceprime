70491b384d92f05962bf23069b57166011923418
Connor French <confrench33@gmail.com>
2024-04-22 16:51:02 -0400

    Bump version: 0.0.2 → 0.0.3

--- spaceprime/__init__.py
+++ spaceprime/__init__.py
@@ -6,7 +6,7 @@ for more information.
 
 __author__ = """Connor French"""
 __email__ = "french.connor.m@gmail.com"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 from spaceprime.utilities import (
